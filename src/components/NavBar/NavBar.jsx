4e872cc71fab8ad8d67ea4d917496e754cced5cd
melkorAMP <melkor@aleph.engineering>
2024-01-11 13:10:34 -0500

    #2 Implement the Langs
    - Create the Langs File

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,11 @@ import "./navstyles.css";
 const NavBar = () => {
   return (
     <nav className="h-[5rem] flex justify-between items-center fixed z-20 w-full nav-bg px-4">
-      <img src="/logo.png" alt="MCS" className="w-[4rem] rounded-full" /> The NavBar
+      <div className="flex justify-center items-center gap-2">
+      <img src="/logo.png" alt="MCS" className="w-[4rem] rounded-full" /> <span className="text-white font-extrabold">MCS</span>
+      </div>
+      <div className=""></div>
+       The NavBar
     </nav>
   );
 };
---
+++ src/langs/langs.js
@@ -0,0 +1,14 @@
+export const Langs = {
+  en: {
+    globalUI: {
+
+    },
+    navbarUI:{
+        services: 'Services',
+        pricing: 'Pricing',
+        about: 'About MCS',
+        contact: 'Contact Us'
+    }
+  },
+  es: {},
+};
