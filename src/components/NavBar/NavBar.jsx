442e635ea559548b276298ca2cb726dfd3d9b698
melkorAMP <melkor@aleph.engineering>
2024-01-11 12:04:36 -0500

    #1 Create the Nav Bar Component
    Create the component and style file

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navstyles.css'
+
+const NavBar = () => {
+  return (
+   <nav className='h-[6rem] flex justify-center items-center'>
+The NavBar
+   </nav>
+  )
+}
+
+export default NavBar
