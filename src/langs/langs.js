6bb2b5243e8faef706ba8ead10d1a4ac96e6a31b
melkorAMP <melkor@aleph.engineering>
2024-01-12 09:48:58 -0500

    #2 Update Langs
    - Create new Lang Entries

--- src/langs/langs.js
+++ src/langs/langs.js
@@ -1,13 +1,31 @@
 export const Langs = {
   en: {
     globalUI: {
-
+      bookOnline: "Book Online to save 20%",
     },
-    navbarUI:{
-        services: 'Services',
-        pricing: 'Pricing',
-        about: 'About MCS',
-        contact: 'Contact Us'
+    navbarUI: {
+      services: "Services",
+      pricing: "Pricing",
+      about: "About MCS",
+      contact: "Contact Us",
+      testimonials: "Testimonials",
+    },
+    heroSectionUI: {
+      bestHaulerAd: "Number one Hauler in all Miami city.",
+      bestHaulerAdText1: "Keep",
+      bestHaulerAdText2: "Your",
+      bestHaulerAdText3: "Space Clean",
+      bestHaulerAdSlogan:
+        "We will be happy to help you to clean and organize your buildings without any effort from your behalf.",
+      monthlyDates: "Monthly Dates",
+      happyClients: "Satisfy Clients",
+      experienceYears: "Years of Experience",
+      greenDeclaration: "We recycle the 60% of what we haul",
+      greenDeclarationSlogan: "Together we can make the planet green."
+    },
+    servicesSectionUI:{
+        title: 'What we Offer',
+        recyclableList: "Recyclable Materials"
     }
   },
   es: {},
