f1359d639a3097912257e67fa098aea3a9690edd
melkorAMP <melkor@aleph.engineering>
2024-01-12 10:01:07 -0500

    #4 Create the Constants
    - Create Constants File
    - Add required Constants

---
+++ src/settings/constants.js
@@ -0,0 +1,26 @@
+import { Langs } from "@/langs/langs";
+
+export const NavLinks = [
+  {
+    text: Langs["en"].navbarUI.services,
+    link: "#Services",
+  },
+  {
+    text: Langs["en"].navbarUI.pricing,
+    link: "#Pricing",
+  },
+  {
+    text: Langs["en"].navbarUI.about,
+    link: "#About",
+  },
+  {
+    text: Langs["en"].navbarUI.contact,
+    link: "#Contact",
+  },
+  {
+    text: Langs["en"].navbarUI.testimonials,
+    link: "#Testimonials",
+  },
+];
+
+
