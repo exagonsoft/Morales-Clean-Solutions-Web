70a683d817985fa4a6f2da413f676a4fe8d84f80
melkorAMP <melkor@aleph.engineering>
2024-02-03 13:18:27 -0500

    #11 Estimator Component
    Implement Component static UI

---
+++ src/components/Estimator/Estimator.jsx
@@ -0,0 +1,22 @@
+import { Langs } from '@/langs/langs'
+import { FaCalculator } from "react-icons/fa";
+import React from 'react'
+
+const Estimator = () => {
+  return (
+    <div className='flex flex-col py-4 px-6 border border-white sm:mx-0 mx-2 rounded-lg bg-cyan-900 shadow-lg'>
+      <h1 className="text-white font-bold sm:text-[1.5rem] text-[1.2rem] align-middle flex justify-center">100% Free of charges</h1>
+      <h1 className="text-white font-bold sm:text-[1rem] text-[.8rem] align-middle flex justify-center text-center">Get a real estimation of the required work cost</h1>
+      <div className="hero-buttons w-full mt-8">
+        <button className="button !w-full">
+          <span className="text-2xl">
+            <FaCalculator />
+          </span>
+          {Langs["en"].globalUI.estimate}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Estimator
