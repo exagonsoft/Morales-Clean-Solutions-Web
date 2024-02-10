098c3b83d67c8a445df1540acd42d2190bca8c09
melkorAMP <melkor@aleph.engineering>
2024-02-10 18:05:39 -0500

    #7 Update build script

--- registerModels.js
+++ registerModels.js
@@ -1,21 +1,30 @@
 // registerModels.js
 
-const fs = require('fs');
-const path = require('path');
-const mongoose = require('mongoose');
+const fs = require("fs");
+const path = require("path");
+const mongoose = require("mongoose");
 
-const modelsDir = path.join(__dirname, '/src/data/models/');
+const modelsDir = path.join(__dirname, "/src/data/models/");
 
 // Get all model files in the models directory
-const modelFiles = fs.readdirSync(modelsDir).filter(file => file.endsWith('Schema.js'));
+const modelFiles = fs
+  .readdirSync(modelsDir)
+  .filter((file) => file.endsWith("Schema.js"));
 
 // Register each model
-modelFiles.forEach(file => {
+modelFiles.forEach((file) => {
   const modelPath = path.join(modelsDir, file);
   require(modelPath);
 });
 
 // Connect to the database
-mongoose.connect('mongodb+srv://exagonsoft:XH6Nuf08qF57Pg7H@msccluster.adammha.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to database'))
-  .catch(error => console.error('Database connection error:', error));
+mongoose
+  .connect(
+    "mongodb+srv://exagonsoft:XH6Nuf08qF57Pg7H@msccluster.adammha.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Connected to database");
+    mongoose.connection.close();
+  })
+  .catch((error) => console.error("Database connection error:", error));
